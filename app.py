0f28f21b1a7e1553b8a647bc61069bb386558e79
chenry22 <112424609+chenry22@users.noreply.github.com>
2023-12-03 16:21:17 -0600

    updated with Docker seperate

---
+++ app.py
@@ -0,0 +1,55 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+model_save = os.path.join(basedir, 'static/allstar_model.sav')
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST']) 
+def process(): 
+    data = request.get_json()
+    prediction = getPrediction(data)
+    return prediction;
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+def getPrediction(arr_in): 
+    log_model = pickle.load(open(model_save, 'rb'))
+
+    features = ['Games Played', 'Games Started', 'MPG', 'FG', 'FGA',
+                '3P', '3PA', '2P', '2PA', 'FT',
+                'FTA', 'ORB', 'DRB', 'TRB', 'AST',
+                'STL', 'BLK', 'PTS', 'TOV', 'Previous Times All-Star',
+                'Win Percent', 'PTS/FGA']
+
+    in_vect = np.array(arr_in)
+    in_vect = in_vect[: len(features)]
+    in_df = pd.DataFrame(in_vect).T
+    in_df.columns = features
+
+    prob = log_model.predict_proba(in_df)[0];
+
+    str_out = "";
+    thrs = 0.75
+
+    if prob[1] > thrs:
+        str_out = "All-Star."
+    else:
+        str_out = "Not All-Star."
+
+    ret = {
+        "prob" : (str(round(prob[1] * 100, 2)) + "%"),
+        "decision" : str_out
+    }
+
+    return ret
